6e3c36e9a70334e878d919c02f0b467f7b812f9e
Azim <yoba.number.one@gmail.com>
2026-01-05 23:48:18 +0300

    feat(nft): inits moodnft.sol and two svg for change of mood

---
+++ src/MoodNft.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MoodNft is ERC721 {
+    string private s_sadSvg;
+    string private s_happySvg;
+    uint256 private s_tokenCounter;
+
+    constructor(
+        string memory sadSvg,
+        string memory happySvg
+    ) ERC721("Mood NFT", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvg = sadSvg;
+        s_happySvg = happySvg;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+}
